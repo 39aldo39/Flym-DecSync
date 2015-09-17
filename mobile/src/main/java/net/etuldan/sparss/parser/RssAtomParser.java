abceba73422fe8c12d6afe3bc4af53f9537223ea
RENAUD-GOUD Arnaud <arnaud.renaudgoud@gmail.com>
2015-09-18 00:51:10 +0200

    #79 : Timezone in date

--- mobile/src/main/java/net/etuldan/sparss/parser/RssAtomParser.java
+++ mobile/src/main/java/net/etuldan/sparss/parser/RssAtomParser.java
@@ -118,12 +118,19 @@ public class RssAtomParser extends DefaultHandler {
 
     private static final String[][] TIMEZONES_REPLACE = {{"MEST", "+0200"}, {"EST", "-0500"}, {"PST", "-0800"}};
 
-    private static final DateFormat[] PUBDATE_DATE_FORMATS = {new SimpleDateFormat("d' 'MMM' 'yy' 'HH:mm:ss", Locale.US),
-            new SimpleDateFormat("d' 'MMM' 'yy' 'HH:mm:ss' 'Z", Locale.US), new SimpleDateFormat("d' 'MMM' 'yy' 'HH:mm:ss' 'z", Locale.US)};
+    private static final DateFormat[] PUBDATE_DATE_FORMATS = {
+            new SimpleDateFormat("d' 'MMM' 'yyyy' 'HH:mm:ss' 'ZZZZ", Locale.US),
+            new SimpleDateFormat("d' 'MMM' 'yy' 'HH:mm:ss' 'Z", Locale.US),
+            new SimpleDateFormat("d' 'MMM' 'yy' 'HH:mm:ss' 'z", Locale.US),
+            new SimpleDateFormat("d' 'MMM' 'yy' 'HH:mm:ss", Locale.US)
+    };
 
-    private static final DateFormat[] UPDATE_DATE_FORMATS = {new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss", Locale.US),
-            new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ssZ", Locale.US), new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss.SSSz", Locale.US),
-            new SimpleDateFormat("yyyy-MM-dd", Locale.US)};
+    private static final DateFormat[] UPDATE_DATE_FORMATS = {
+            new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss.SSSz", Locale.US),
+            new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ssZ", Locale.US),
+            new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss", Locale.US),
+            new SimpleDateFormat("yyyy-MM-dd", Locale.US)
+    };
 
     private final Date mRealLastUpdateDate;
     private final String mId;
