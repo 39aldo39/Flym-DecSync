73f0b5038a75bb825a4ee72b83cb13bd7e5954de
Aldo Gunsing <dev@aldogunsing.nl>
2020-08-27 21:21:33 +0200

    Update libs

--- mobile/src/androidTest/java/org/decsync/sparss/utils/SampleAndroidTest.java
+++ mobile/src/androidTest/java/org/decsync/sparss/utils/SampleAndroidTest.java
@@ -1,32 +1,17 @@
 package org.decsync.sparss.utils;
 
-import android.content.Context;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import android.test.InstrumentationTestCase;
 
-import org.decsync.sparss.MainApplication;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
-public class SampleAndroidTest extends InstrumentationTestCase {
-
-    Context mContext;
-
-    @Before
-    public void setUp() {
-        mContext = getInstrumentation().getContext();
-        MainApplication.setContext(mContext);
-    }
+public class SampleAndroidTest {
 
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
 
